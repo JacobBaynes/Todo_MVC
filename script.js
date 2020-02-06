178d0babe3fdcc55b9a134834867e1dcd4f451d7
Jacob Andrew Baynes <jacob.baynes@icloud.com>
2020-02-06 15:02:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ Managaes the data of the application
 
 class Model {
     constructor() {
-    // The state of the model, an array of todo objects, prepopulated with
-    // some data
-        // this.todos = [
-        //     { id: 1, text: 'Run a marathon', complete: false },
-        //     { id: 2, text: 'Plant a garden', complete: false }
-        // ]
-
+    
         this.todos = JSON.parse(localStorage.getItem('todos')) || []
     }
 
